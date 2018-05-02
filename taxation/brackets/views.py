b91c75dfe499378bd36482da80d29ead63eabd02
github username <github email address>
2018-05-02 03:43:54 -0400

    added materialize, make connnect

--- taxation/brackets/views.py
+++ taxation/brackets/views.py
@@ -4,6 +4,7 @@ from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
 from .models import UserInfo
 
+
 def index(request):
     latest_salary_list = UserInfo.objects.order_by('salary')
     template = loader.get_template('brackets/index.html')
@@ -11,7 +12,7 @@ def index(request):
         'latest_salary_list': latest_salary_list,
     }
     return HttpResponse(template.render(context, request))
-
+#fix so above is applicable to form...no more salary list.
 def display(request, user_info_id):
     user_info = get_object_or_404(UserInfo, pk=user_info_id)
     return render(request, 'brackets/display.html', {'user_info': user_info})
