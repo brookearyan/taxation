790083b0c3767cc0c8d14a7f83148fd667b59584
github username <github email address>
2018-05-02 00:54:07 -0400

    started views, routes work. tested

--- taxation/brackets/urls.py
+++ taxation/brackets/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('<int:user_info_id>/', views.display, name='display'),
 ]
--- taxation/brackets/views.py
+++ taxation/brackets/views.py
@@ -1,5 +1,7 @@
 from django.http import HttpResponse
 
-
-def index(request, federal_id):
+def index(request):
     return HttpResponse("HEY GURL HEYYYs")
+
+def display(request, user_info_id):
+    return HttpResponse("This is user number: %s" % user_info_id)
