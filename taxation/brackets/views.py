ec77dcb99954991bcd8fe29bd59d461a2c631261
github username <github email address>
2018-05-02 01:22:12 -0400

    templates connected, work

--- taxation/brackets/views.py
+++ taxation/brackets/views.py
@@ -1,7 +1,14 @@
 from django.http import HttpResponse
+from django.template import loader
+from .models import UserInfo
 
 def index(request):
-    return HttpResponse("HEY GURL HEYYYs")
+    latest_salary_list = UserInfo.objects.order_by('salary')
+    template = loader.get_template('brackets/index.html')
+    context = {
+        'latest_salary_list': latest_salary_list,
+    }
+    return HttpResponse(template.render(context, request))
 
 def display(request, user_info_id):
     return HttpResponse("This is user number: %s" % user_info_id)
