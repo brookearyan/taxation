9544a683344337b52917fb5cb121ab3169cc29f1
github username <github email address>
2018-05-02 01:55:21 -0400

    changed routes, namespaced, works

--- taxation/brackets/urls.py
+++ taxation/brackets/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 
 from . import views
 
+app_name = 'brackets'
 urlpatterns = [
     path('', views.index, name='index'),
     path('<int:user_info_id>/', views.display, name='display'),
