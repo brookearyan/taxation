ca7d1ad3207a551a9ac98a17a3ad316c6b5be45d
github username <github email address>
2018-05-03 14:38:40 -0400

    related object classes state fed has many users

--- taxation/brackets/models.py
+++ taxation/brackets/models.py
@@ -1,17 +1,9 @@
 from django.db import models
 
-# Create your models here.
-class UserInfo(models.Model):
-    SINGLE_OR_MARRIED = (
-        ('S', 'single'),
-        ('M', 'married'),
-    )
-    zip_code = models.IntegerField(default=10016)
-    salary = models.DecimalField(max_digits=15, decimal_places=2, default=0)
-    marital_status = models.CharField(max_length=1, choices=SINGLE_OR_MARRIED)
 
 # abstract class inherited by state & federal brackets
 class Brackets(models.Model):
+
     greater_than = models.DecimalField(max_digits=15, decimal_places=2, default=0)
     last_bkt = models.DecimalField(max_digits=15, decimal_places=2, default=0)
     init_tax = models.DecimalField(max_digits=15, decimal_places=2, default=0)
@@ -25,3 +17,15 @@ class State(Brackets):
 
 class Federal(Brackets):
     federal_id = models.CharField(primary_key=True, max_length=10)
+# Create your models here.
+
+class UserInfo(models.Model):
+    SINGLE_OR_MARRIED = (
+        ('S', 'single'),
+        ('M', 'married'),
+    )
+    zip_code = models.IntegerField(default=10016)
+    salary = models.DecimalField(max_digits=15, decimal_places=2, default=0)
+    marital_status = models.CharField(max_length=1, choices=SINGLE_OR_MARRIED)
+    state_bracket = models.ForeignKey(State, related_name='state_bracket', to_field='state_id', null=True, on_delete=models.SET_NULL)
+    fed_bracket = models.ForeignKey(Federal, related_name='fed_bracket', to_field='federal_id', null=True, on_delete=models.SET_NULL)
