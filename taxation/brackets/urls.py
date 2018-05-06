b27bf71664a4faca48f0f230616a55a36301589d
github username <github email address>
2018-05-06 17:48:22 -0400

    cleaned up views a bit:

--- taxation/brackets/urls.py
+++ taxation/brackets/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('<int:user_info_id>/', views.display, name='display'),
     path('new/', views.new, name='form'),
-    path('<int:user_info_id/', views.new, name='new'),
+    path('<int:user_info_id/new', views.new, name='new'),
 ]
--- taxation/brackets/views.py
+++ taxation/brackets/views.py
@@ -6,13 +6,8 @@ from .models import UserInfo
 from .forms import New
 
 def index(request):
-    latest_salary_list = UserInfo.objects.order_by('salary')
-    template = loader.get_template('brackets/index.html')
-    context = {
-        'latest_salary_list': latest_salary_list,
-    }
-    return HttpResponse(template.render(context, request))
-#fix so above is applicable to form...no more salary list.
+    return render(request, 'brackets/index.html')
+    
 def display(request, user_info_id):
     user_info = get_object_or_404(UserInfo, pk=user_info_id)
     return render(request, 'brackets/display.html', {'user_info': user_info})
@@ -20,11 +15,11 @@ def display(request, user_info_id):
 def new(request):
     if request.method == 'POST':
         form = New(request.POST)
-        if form.is_valid():
-            zip_code = form.cleaned_data['zip_code']
-            salary = form.cleaned_data['salary']
-            marital_status = form.cleaned_data['marital_status']
-            print(zip_code, salary, marital_status)
+        return HttpResponseRedirect()
+        # if form.is_valid():
+        #     zip_code = form.cleaned_data['zip_code']
+        #     salary = form.cleaned_data['salary']
+        #     marital_status = form.cleaned_data['marital_status']
 
     else:
         form = New()
