1bd5e75c3fc84f715cc12d8937a13715f85a081c
github username <github email address>
2018-05-08 17:51:40 -0400

    get req to zip api working, must change models and migrate w city state etc attrs

--- taxation/brackets/views.py
+++ taxation/brackets/views.py
@@ -5,14 +5,6 @@ from .models import UserInfo
 from .forms import New
 import coreapi
 
-def city_and_state(zip_code):
-    zip_code = str(zip_code)
-    api_key = 'jYDPzTUAvlLWJMPe241qEXPVJKvIT83FLp5IBV9V7UxPu80KFCo7JyBzUqCoxdm3'
-    request_url = 'https://www.zipcodeapi.com/rest/'+ api_key + '/info.json/'+ zip_code +'/degrees'
-    client = coreapi.Client()
-    result = client.get(request_url)
-    return result
-
 def index(request):
     if request.method == 'POST':
         form = New(request.POST)
@@ -23,10 +15,8 @@ def index(request):
                 marital_status=form.cleaned_data['marital_status']
                 )
             user_info.save()
-            result = city_and_state(user_info.zip_code)
-            city = result[3][1]
 
-        return HttpResponseRedirect(reverse('brackets:display', args=(user_info.id, city)))
+        return HttpResponseRedirect(reverse('brackets:display', args=(user_info.id,)))
     else:
         form = New()
         return render(request, 'brackets/index.html', {'form':form})
@@ -37,3 +27,13 @@ def display(request, user_info_id):
     except UserInfo.DoesNotExist:
         raise Http404("this user does not exist")
     return render(request, 'brackets/display.html', {'user_info': user_info})
+
+def city_and_state(zip_code):
+    zip_code = str(zip_code)
+    api_key = 'jYDPzTUAvlLWJMPe241qEXPVJKvIT83FLp5IBV9V7UxPu80KFCo7JyBzUqCoxdm3'
+    request_url = 'https://www.zipcodeapi.com/rest/'+ api_key + '/info.json/'+ zip_code +'/degrees'
+    client = coreapi.Client()
+    schema = client.get(request_url)
+    params = { city: 'city', state: 'state' }
+    result = client.action(schema, params)
+    return result
