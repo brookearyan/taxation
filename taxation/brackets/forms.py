de71efcd6bdd93e9564b314091e358cc21c4ee81
github username <github email address>
2018-05-18 14:45:00 -0400

    added a couple form fields and JQuery script

--- taxation/brackets/forms.py
+++ taxation/brackets/forms.py
@@ -5,7 +5,66 @@ SINGLE_OR_MARRIED = (
     ('M', 'married'),
 )
 
+US_STATES = (
+    ('AL', 'Alabama'),
+    ('AK', 'Alaska'),
+    ('AZ', 'Arizona'),
+    ('AR', 'Arkansas'),
+    ('CA', 'California'),
+    ('CO', 'Colorado'),
+    ('CT', 'Connecticutt'),
+    ('DE', 'Delaware'),
+    ('DC', 'District of Columbia'),
+    ('FL', 'Florida'),
+    ('GA', 'Georgia'),
+    ('HI', 'Hawaii'),
+    ('ID', 'Idaho'),
+    ('IL', 'Illinois'),
+    ('IN', 'Indiana'),
+    ('IA', 'Iowa'),
+    ('KS', 'Kansas'),
+    ('KY', 'Kentucky'),
+    ('LA', 'Louisiana'),
+    ('ME', 'Maine'),
+    ('MD', 'Maryland'),
+    ('MA', 'Massachusetts'),
+    ('MI', 'Michigan'),
+    ('MN', 'Minnesota'),
+    ('MI', 'Mississippi'),
+    ('MO', 'Missouri'),
+    ('MT', 'Montana'),
+    ('NE', 'Nebaska'),
+    ('NV', 'Nevada'),
+    ('NH', 'New Hampshire'),
+    ('NJ', 'New Jersey'),
+    ('NM', 'New Mexico'),
+    ('NY', 'New York'),
+    ('NC', 'North Carolina'),
+    ('ND', 'North Dakota'),
+    ('OH', 'Ohio'),
+    ('OK', 'Oklahoma'),
+    ('OR', 'Oregon'),
+    ('PA', 'Pennsylvania'),
+    ('RI', 'Rhode Island'),
+    ('SC', 'South Carolina'),
+    ('SD', 'South Dakota'),
+    ('TN', 'Tennessee'),
+    ('TX', 'Texas'),
+    ('UT', 'Utah'),
+    ('VT', 'Vermont'),
+    ('VA', 'Virginia'),
+    ('WA', 'Washington'),
+    ('WV', 'West Virginia'),
+    ('WI', 'Wisconsin'),
+    ('WY', 'Wyoming'),
+)
+
 class New(forms.Form):
-    zip_code = forms.IntegerField()
     salary = forms.DecimalField(max_digits=15, decimal_places=2)
-    marital_status = forms.ChoiceField(widget=forms.RadioSelect(attrs={'class': 'with-gap'}), choices=SINGLE_OR_MARRIED)
+    marital_status = forms.ChoiceField(choices=SINGLE_OR_MARRIED)
+    state = forms.ChoiceField(choices=US_STATES)
+
+
+    # zip_code = forms.IntegerField()
+
+###MAKE DC TAX STATE NOT LOCAL
