13919ce3d30f8162516e8d5fbab6fb9cdba725c4
github username <github email address>
2018-06-26 13:00:51 -0500

    removed zip, edited city and state works but form is rendering strange still

--- taxation/brackets/models.py
+++ taxation/brackets/models.py
@@ -75,11 +75,10 @@ class UserInfo(models.Model):
         ('WI', 'Wisconsin'),
         ('WY', 'Wyoming'),
     )
-    zip_code = models.IntegerField(default=10016)
     salary = models.DecimalField(max_digits=15, decimal_places=2, default=0)
     marital_status = models.CharField(max_length=7, choices=SINGLE_OR_MARRIED)
     state_bracket = models.ForeignKey(State, related_name='state_bracket', to_field='state_id', null=True, on_delete=models.SET_NULL)
     fed_bracket = models.ForeignKey(Federal, related_name='fed_bracket', to_field='federal_id', null=True, on_delete=models.SET_NULL)
-    city = models.CharField(max_length=50, null=True)
-    state = models.CharField(max_length=50, null=True)
+    city = models.CharField(max_length=50)
+    state = models.CharField(max_length=50, choices=US_STATES)
     total_taxes = models.DecimalField(max_digits=15, decimal_places=2, null=True)
