5a0ba11c504ec8c3f26f36e08b6a79c8aed5766b
github username <github email address>
2018-08-15 13:34:33 -0400

    logic file

--- taxation/brackets/apps.py
+++ taxation/brackets/apps.py
@@ -1,5 +1,4 @@
 from django.apps import AppConfig
 
-
 class BracketsConfig(AppConfig):
     name = 'brackets'
--- taxation/brackets/models.py
+++ taxation/brackets/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+# from logic import *
 
 
 # abstract class inherited by state & federal brackets
